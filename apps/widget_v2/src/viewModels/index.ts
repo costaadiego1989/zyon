1f410836f812b92f1808bf4b324078143f9ccfc8
Diego <costaadiego1989@gmail.com>
2026-08-29 01:56:10 -0300

    feat(widget): add error-handler service and viewmodel types foundation

---
+++ apps/widget_v2/src/viewModels/index.ts
@@ -0,0 +1,5 @@
+export { type ChatViewModelInterface, type SupportViewModelInterface, type PaymentViewModelInterface } from "./types";
+export { type Message, type SupportMessage, type FaqItem } from "./types";
+export { useChatViewModel } from "./useChatViewModel";
+export { useSupportViewModel } from "./useSupportViewModel";
+export { usePaymentViewModel } from "./usePaymentViewModel";
